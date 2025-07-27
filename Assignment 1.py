39c00513f79e42d803318c4901d8fcd6299b700d
vimalk14 <iamvimalkant@gmail.com>
2025-07-27 14:33:37 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,19 @@
+# Basic Mathematical Operations
+n1=input('Enter the First Number: ')
+n2=input('Enter the Second Number: ')
+a=int(n1)+int(n2)
+s=int(n1)-int(n2)
+m=int(n1)*int(n2)
+d=int(n1)/int(n2)
+print('Addition :',a)
+print('Subtraction :',s)
+print('Multiplication: ',m)
+print('Division: ',d)
+
+# Personalized Greeting
+f = input('Enter your first name: ')
+l = input('Enter your last name: ')
+name = f + " " + l
+print('Hello,', name, '! Welcome to the Python Program.')
+
+
